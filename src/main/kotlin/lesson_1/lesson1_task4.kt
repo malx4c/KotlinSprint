0eff8142073e3cbe0dbb01abffd86a7202c814d2
Aleksandr Volynski <al.volynski@ariscard.net>
2025-03-27 01:39:08 +0300

    KS-1-4 fix partOfDay

--- src/main/kotlin/lesson_1/lesson1_task4.kt
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -2,11 +2,8 @@ package org.example.lesson_1
 
 fun main () {
     val length: Long = 40868600000
-    val length1 = 40868600000
-    println(length1::class) //long (Kotlin reflection is not available)
-
     val age: Byte = 27
-    val partOfDay: Double = 0.075
+    val partOfDay: Float = 0.075F
     val seconds: Short = 6480
     val partOfYear: Double = 2.0547945205479453E-4
     val apogee: Int = 327000
