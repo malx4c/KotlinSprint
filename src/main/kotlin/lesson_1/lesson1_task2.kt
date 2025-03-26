a9ef95d2ef9dca560b9bf197d011c2ffb7c7c55f
Aleksandr Volynski <al.volynski@ariscard.net>
2025-03-26 21:18:11 +0300

    KS-1-2 simulate server log

---
+++ src/main/kotlin/lesson_1/lesson1_task2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson_1
+
+fun main () {
+    val numberOfOrders = 75
+    val messageThank = "Спасибо за покупку!"
+    var numberOfEmployees = 2000
+
+    println(numberOfOrders)
+    println(messageThank)
+
+//    println(numberOfEmployees)
+    numberOfEmployees = 1999
+    println(numberOfEmployees)
+}
