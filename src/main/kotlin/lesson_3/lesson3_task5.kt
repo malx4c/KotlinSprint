b29e9c5f52eb809d696c53e439c5abe6fcb7aac7
Aleksandr Volynski <al.volynski@ariscard.net>
2025-03-28 16:48:49 +0300

    KS-3-5 fix

--- src/main/kotlin/lesson_3/lesson3_task5.kt
+++ src/main/kotlin/lesson_3/lesson3_task5.kt
@@ -2,18 +2,18 @@ package lesson_3
 
 fun main() {
     val receivedString = "D2-D4;0"
-    val source = receivedString.slice(SOURCE_INDEX_BEGIN..SOURCE_INDEX_END)
-    val target = receivedString.slice(TARGET_INDEX_BEGIN..TARGET_INDEX_END)
-    val step = receivedString.substring(STEP_INDEX_BEGIN, STEP_INDEX_END)
+
+    val receivedData = receivedString.split("-",";")
+    val source = receivedData[SOURCE_INDEX]
+    val target = receivedData[TARGET_INDEX]
+    val step = receivedData[STEP_INDEX]
 
     println(source)
     println(target)
     println(step)
 }
 
-const val SOURCE_INDEX_BEGIN = 0
-const val SOURCE_INDEX_END = 1
-const val TARGET_INDEX_BEGIN = 3
-const val TARGET_INDEX_END = 4
-const val STEP_INDEX_BEGIN = 6
-const val STEP_INDEX_END = 7
+const val SOURCE_INDEX = 0
+const val TARGET_INDEX = 1
+const val STEP_INDEX = 2
+
