b1a4670354a9d84eb423f14851f55d81a63f0a23
Aleksandr Volynski <al.volynski@ariscard.net>
2025-03-29 18:37:55 +0300

    KS-4-2 fix

--- src/main/kotlin/lesson_4/lesson4_2.kt
+++ src/main/kotlin/lesson_4/lesson4_2.kt
@@ -1,17 +1,17 @@
 package lesson_4
 
 fun main() {
-    val weightMin = 35f
-    val weightMax = 100f
-    val volumeMax = 100f
-
     var weight = 20
     var volume = 80
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${weight > weightMin && weight <= weightMax && volume < volumeMax}")
+    var result = weight > WEIGHT_MIN && weight <= WEIGHT_MAX && volume < VOLUME_MAX
+    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': $result")
 
     weight = 50
     volume = 100
-    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': ${weight > weightMin && weight <= weightMax && volume < volumeMax}")
-
+    result = weight > WEIGHT_MIN && weight <= WEIGHT_MAX && volume < VOLUME_MAX
+    println("Груз с весом $weight кг и объемом $volume л соответствует категории 'Average': $result")
 }
 
+const val WEIGHT_MIN = 35f
+const val WEIGHT_MAX = 100f
+const val VOLUME_MAX = 100f
