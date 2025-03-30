5fa0f13acbf6c49de3c5483ad905ea34a088f353
Aleksandr Volynski <al.volynski@ariscard.net>
2025-03-30 22:38:33 +0300

    KS-5-1 fix

--- src/main/kotlin/lesson_5/lesson5_task1.kt
+++ src/main/kotlin/lesson_5/lesson5_task1.kt
@@ -1,13 +1,13 @@
 package lesson_5
 
 fun main() {
-    val additiveNumber1: Int = (Math.random() * 10).toInt()
-    val additiveNumber2: Int = (Math.random() * 10).toInt()
+    val number1 = (0..10).random()
+    val number2 =  (0..10).random()
+
+    print("Сколько будет $number1 + $number2 = ")
     val answer = readlnOrNull()?.toInt()
 
-    print("Сколько будет $additiveNumber1 + $additiveNumber2 = ")
-
-    if (answer == additiveNumber1 + additiveNumber2)
+    if (answer == number1 + number2)
         println ("Добро пожаловать!")
     else
         println ("Доступ запрещен.")
