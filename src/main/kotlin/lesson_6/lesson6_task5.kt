01591c06a6605380781372e1be5e1978b2381025
Aleksandr Volynski <al.volynski@ariscard.net>
2025-04-02 17:25:22 +0300

    KS-6-5 fix

--- src/main/kotlin/lesson_6/lesson6_task5.kt
+++ src/main/kotlin/lesson_6/lesson6_task5.kt
@@ -2,10 +2,11 @@ package lesson_6
 
 fun main () {
     var isAuth = false
+    val range = 1..9
 
     for (attempt in 1..NUMBER_OF_ATTEMPTS) {
-        val number1 = (1..9).random()
-        val number2 = (1..9).random()
+        val number1 = range.random()
+        val number2 = range.random()
 
         print("Сколько будет $number1 + $number2 = ")
         val answer = readlnOrNull()?.toInt()
