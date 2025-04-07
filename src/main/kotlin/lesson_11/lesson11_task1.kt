9884b5e098c8ad1fae999034c53c3280403a7347
Aleksandr Volynski <al.volynski@ariscard.net>
2025-04-07 13:30:42 +0300

    KS-11-1 user class

---
+++ src/main/kotlin/lesson_11/lesson11_task1.kt
@@ -0,0 +1,36 @@
+package lesson_11.task1
+
+fun main() {
+    val user1 = User (
+        id = 1,
+        login = "u1login",
+        pass = "u1pass",
+        email = "user1@user.xyz"
+    )
+
+    val user2 = User (
+        id = 2,
+        login = "u2login",
+        pass = "u2pass",
+        email = "user2@user.xyz"
+    )
+
+    println(user1.id)
+    println(user1.login)
+    println(user1.pass)
+    println(user1.email)
+
+    println(user2.id)
+    println(user2.login)
+    println(user2.pass)
+    println(user2.email)
+}
+
+class User(
+    val id: Int,
+    val login: String,
+    val pass: String,
+    val email: String,
+) {
+
+}
