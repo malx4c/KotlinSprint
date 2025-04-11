8504724ab97312569e9dd527293a8a4ac791c327
Aleksandr Volynski <al.volynski@ariscard.net>
2025-04-11 22:44:18 +0300

    KS-13-5 fix

--- src/main/kotlin/lesson_13/lesson13_task5.kt
+++ src/main/kotlin/lesson_13/lesson13_task5.kt
@@ -1,12 +1,5 @@
 package lesson_13.task5
 
-/*
-Задача 5* к Уроку 13
-Скопируй класс из предыдущей задачи (без логики заполнения телефонной книги) и измени его название для избегания конфликтов.
-Для получения номера программа считывает ввод пользователя с типом String по умолчанию. Приведи строку к типу Long.
-Если пользователь ввел что-либо, кроме цифр — программа падает.
-Необходимо избежать падения программы при попытке приведения строки к типу Long. Отлови ошибку и выведи ее название в консоль.
-* */
 fun main() {
     for (i in 1..NUMBER_CONTACTS) {
         val phone = getPhoneNumber()
@@ -26,20 +19,4 @@ fun getPhoneNumber(): Long? {
     }
 }
 
-class Contact(
-    val name: String,
-    val phoneNumber: Int,
-    val company: String?
-) {
-
-    fun printInfo() {
-        var info = "- Имя: $name\n"
-        info += "- Номер телефона: $phoneNumber\n"
-        info += "- Компания: ${company ?: BLANK_TEXT}\n"
-
-        println(info)
-    }
-}
-
-const val BLANK_TEXT = "<не указано>"
 const val NUMBER_CONTACTS = 5
