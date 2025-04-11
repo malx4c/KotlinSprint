0b5860179f6ce8102e01d673db7e4e6acf11f219
Aleksandr Volynski <al.volynski@ariscard.net>
2025-04-11 22:36:53 +0300

    KS-13-3 fix

--- src/main/kotlin/lesson_13/lesson13_task3.kt
+++ src/main/kotlin/lesson_13/lesson13_task3.kt
@@ -1,31 +1,23 @@
 package lesson_13.task3
 
-/*
-Задача 3 к Уроку 13
-Скопируй класс из предыдущей задачи и измени его название для избегания конфликтов.
-Для того, чтобы взаимодействовать со всеми записями телефонной книги, как с одним целым, их нужно объединить в список.
-- создай и инициализируй (одновременно) список с 5 объектами класса-контакта;
-- 2 контакта должны принимать null для поля компании;
-- 1 контакт должен иметь название компании “null”;
-- выведи список всех существующих компаний, которые есть в телефонной книге.
- */
 fun main() {
-    val contactList: List<Contact> = listOf(
-        Contact("Вася", 12345, null),
-        Contact("Петя", 67890, null),
-        Contact("Маша", 54321, "null"),
-        Contact("Ира", 98765, "ИП Ира"),
-        Contact("Коля", 19283, "ООО Коля"),
-    )
+    val contactList: List<Contact> =
+        listOf(
+            Contact("Вася", 12345, null),
+            Contact("Петя", 67890, null),
+            Contact("Маша", 54321, "null"),
+            Contact("Вова", 54321, "null"),
+            Contact("Ира", 98765, "ИП Ира"),
+            Contact("Коля", 19283, "ООО Коля"),
+            Contact("Коля", 19283, "ООО Коля"),
+        )
 
-    val companies = contactList.mapNotNull { it.company }
+    val companies = contactList.mapNotNull { it.company }.distinct()
     companies.forEach { println(it) }
 }
 
 class Contact(
     val name: String,
     val phoneNumber: Long,
-    val company: String?
+    val company: String?,
 )
-
-const val BLANK_TEXT = "<не указано>"
