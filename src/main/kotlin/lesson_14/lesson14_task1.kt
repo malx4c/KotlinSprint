81306a605a234ad245f6850c81575ba3dfc03adb
Aleksandr Volynski <al.volynski@ariscard.net>
2025-04-13 14:48:33 +0300

    KS-14-1 fix

--- src/main/kotlin/lesson_14/lesson14_task1.kt
+++ src/main/kotlin/lesson_14/lesson14_task1.kt
@@ -2,28 +2,30 @@ package lesson_14
 
 fun main() {
     val liner = Liner("Liner", 30, 1500)
-    val cargo = CargoShip("CargoShip", 10, 2000)
-    val iceBreaker = IceBreaker("IceBreaker", 5, true)
+    val cargo = CargoShip("CargoShip")
+    val iceBreaker = IceBreaker("IceBreaker")
 }
 
 open class Liner(
     val name: String = DEF_NAME,
-    val speed: Int = DEF_SPEED,
+    open val speed: Int = DEF_SPEED_LINER,
     val numberPassengers: Int = DEF_NUMBER_PASSENGERS,
 )
 
 class CargoShip(
     name: String,
-    speed: Int,
-    val liftingCapacity: Int,
-) : Liner(name, speed)
+    override val speed: Int = DEF_SPEED_CARGO_SHIP,
+    val liftingCapacity: Int = DEF_LIFTING_CAPACITY,
+) : Liner(name)
 
 class IceBreaker(
     name: String,
-    speed: Int,
+    override val speed: Int = 5,
     val canCrushIce: Boolean = true,
-) : Liner(name, speed)
+) : Liner(name)
 
 const val DEF_NAME = "Ship"
-const val DEF_SPEED = 25
+const val DEF_SPEED_LINER = 25
+const val DEF_SPEED_CARGO_SHIP = 25
 const val DEF_NUMBER_PASSENGERS = 25
+const val DEF_LIFTING_CAPACITY = 200
