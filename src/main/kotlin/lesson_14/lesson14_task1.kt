4dea21b3156b2425533eeac84af866d96a298e89
Aleksandr Volynski <al.volynski@ariscard.net>
2025-04-13 19:42:31 +0300

    KS-14-1 fix

--- src/main/kotlin/lesson_14/lesson14_task1.kt
+++ src/main/kotlin/lesson_14/lesson14_task1.kt
@@ -1,31 +1,32 @@
 package lesson_14
 
 fun main() {
-    val liner = Liner("Liner", 30, 1500)
-    val cargo = CargoShip("CargoShip")
+    val liner = Liner("Liner", numberPassengers = 2000)
+    val cargoShip = CargoShip("CargoShip")
     val iceBreaker = IceBreaker("IceBreaker")
 }
 
 open class Liner(
-    val name: String = DEF_NAME,
-    open val speed: Int = DEF_SPEED_LINER,
+    val name: String,
+    val speed: Int = DEF_SPEED,
+    val liftingCapacity: Int = DEF_LIFTING_CAPACITY,
     val numberPassengers: Int = DEF_NUMBER_PASSENGERS,
+    val canCrushIce: Boolean = false,
 )
 
 class CargoShip(
     name: String,
-    override val speed: Int = DEF_SPEED_CARGO_SHIP,
-    val liftingCapacity: Int = DEF_LIFTING_CAPACITY,
-) : Liner(name)
+    speed: Int = 15,
+    liftingCapacity: Int = 1000,
+) : Liner(name, speed, liftingCapacity)
 
 class IceBreaker(
     name: String,
-    override val speed: Int = 5,
-    val canCrushIce: Boolean = true,
-) : Liner(name)
+    speed: Int = 10,
+    liftingCapacity: Int = 200,
+    canCrushIce: Boolean = true,
+) : Liner(name, speed, liftingCapacity)
 
-const val DEF_NAME = "Ship"
-const val DEF_SPEED_LINER = 25
-const val DEF_SPEED_CARGO_SHIP = 25
+const val DEF_SPEED = 25
 const val DEF_NUMBER_PASSENGERS = 25
-const val DEF_LIFTING_CAPACITY = 200
+const val DEF_LIFTING_CAPACITY = 100
