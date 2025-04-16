39081a2517017eb08b95151b947cfb626100f89a
Aleksandr Volynski <al.volynski@ariscard.net>
2025-04-16 12:44:29 +0300

    KS-15-2 fix

--- src/main/kotlin/lesson_15/lesson15_task2.kt
+++ src/main/kotlin/lesson_15/lesson15_task2.kt
@@ -3,31 +3,45 @@ package lesson_15
 fun main() {
     val temperature = Temperature(25.5F)
     val precipitationAmount = PrecipitationAmount(10)
+    val fake = Fake(name = "", value = null)
+
     val server = WeatherServer()
 
     server.sendWeatherData(temperature)
     server.sendWeatherData(precipitationAmount)
+    server.sendWeatherData(fake)
 }
 
 abstract class WeatherStationStats {
     abstract val name: String
-    abstract val value: Any
+    abstract val value: Any?
+
+    fun isValid() = name.isNotEmpty() && value != null
 }
 
+class Fake(
+    override val name: String,
+    override val value: Any?,
+) : WeatherStationStats()
+
 class Temperature(
-    override val value: Float,
+    override val value: Float?,
 ) : WeatherStationStats() {
     override val name = "Температура"
 }
 
 class PrecipitationAmount(
-    override val value: Int,
+    override val value: Int?,
 ) : WeatherStationStats() {
     override val name = "Количество осадков"
 }
 
 class WeatherServer {
     fun sendWeatherData(weatherData: WeatherStationStats) {
-        println("Датчик <${weatherData.name}>: ${weatherData.value}")
+        if (weatherData.isValid()) {
+            println("Датчик <${weatherData.name}>: ${weatherData.value}")
+        } else {
+            println("Данные не распознаны")
+        }
     }
 }
