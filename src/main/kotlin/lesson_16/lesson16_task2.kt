bd43d360dea9fb5df24c96a8b66e8a3b8d2abb78
Aleksandr Volynski <al.volynski@ariscard.net>
2025-04-18 00:23:01 +0300

    KS-16-2 fix

--- src/main/kotlin/lesson_16/lesson16_task2.kt
+++ src/main/kotlin/lesson_16/lesson16_task2.kt
@@ -1,17 +1,17 @@
 package lesson_16
 
 fun main() {
-    val circle = Circle(10f)
+    val circle = Circle(10.0)
     println("Площадь: ${circle.getSquare()}")
     println("Длина: ${circle.getLength()}")
 }
 
 class Circle(
-    private val radius: Float,
+    private val radius: Double,
 ) {
-    fun getSquare(): Float = PI * radius * radius
+    fun getSquare(): Double = PI * Math.pow(radius, 2.0)
 
-    fun getLength(): Float = 2 * PI * radius
+    fun getLength(): Double = 2 * PI * radius
 }
 
 private const val PI = 3.14f
