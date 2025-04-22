787c9c9459261812933bcebf9569d74a866eee8d
Aleksandr Volynski <al.volynski@ariscard.net>
2025-04-22 10:41:28 +0300

    KS-18-1 fix

--- src/main/kotlin/lesson_18/lesson18_task1.kt
+++ src/main/kotlin/lesson_18/lesson18_task1.kt
@@ -1,34 +1,21 @@
 package lesson_18
 
 fun main() {
-    val orders: List<Order> =
-        listOf(
-            OrderSingle(1, "стол"),
-            OrderMulti(2, listOf("стул", "ложка", "вилка")),
-        )
-    orders.forEach { it.getInfo() }
+    val order = Order(1)
+    order.addProduct("стол")
+
+    val order2 = Order(2)
+    order2.addProduct(listOf("стул", "ложка", "вилка"))
 }
 
 open class Order(
     val numberOrder: Int,
 ) {
-    open fun getInfo() {}
-}
-
-class OrderSingle(
-    numberOrder: Int,
-    val product: String,
-) : Order(numberOrder) {
-    override fun getInfo() {
-        println("Заказан товар: $product")
+    fun addProduct(product: String) {
+        println("Заказ N $numberOrder содержит <$product>")
     }
-}
 
-class OrderMulti(
-    numberOrder: Int,
-    val products: List<String>,
-) : Order(numberOrder) {
-    override fun getInfo() {
-        println("Заказан следующие товары : ${products.joinToString(", ")}")
+    fun addProduct(products: List<String>) {
+        println("Заказ N $numberOrder содержит <${products.joinToString(", ")}>")
     }
 }
