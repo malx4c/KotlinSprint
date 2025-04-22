5e54efe5b95f2c2bf61f617abe40e54597706e11
Aleksandr Volynski <al.volynski@ariscard.net>
2025-04-22 10:12:18 +0300

    KS-19-1 fix

--- src/main/kotlin/lesson_19/lesson19_task1.kt
+++ src/main/kotlin/lesson_19/lesson19_task1.kt
@@ -2,22 +2,14 @@ package lesson_19
 
 fun main() {
     println("Список рыб:")
-    for (fish in Fish.entries) println(fish.getName())
+    for (fish in Fish.entries) println(fish.nameFish)
 }
 
-enum class Fish {
-    GUPPY {
-        override fun getName() = "Гуппи"
-    },
-    ANGEL_FISH {
-        override fun getName() = "Скалярия"
-    },
-    GOLD_FISH {
-        override fun getName() = "Золотая рыбка"
-    },
-    SIAMESE_FIGHTING_FISH {
-        override fun getName() = "Петушок"
-    }, ;
-
-    abstract fun getName(): String
+enum class Fish(
+    val nameFish: String,
+) {
+    GUPPY("Гуппи"),
+    ANGEL_FISH("Скалярия"),
+    GOLD_FISH("Золотая рыбка"),
+    SIAMESE_FIGHTING_FISH("Петушок"),
 }
