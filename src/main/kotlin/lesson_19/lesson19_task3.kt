faebaa2e013d29b8185746f36a43e5b4ed275491
Aleksandr Volynski <al.volynski@ariscard.net>
2025-04-23 22:08:46 +0300

    KS-19-3 spaceship

---
+++ src/main/kotlin/lesson_19/lesson19_task3.kt
@@ -0,0 +1,19 @@
+package lesson_19
+
+fun main() {
+    val ship = SpaceShip()
+    ship.shootBackAsteroid()
+}
+
+class SpaceShip {
+    fun takeOff() {
+    }
+
+    fun land() {
+        TODO()
+    }
+
+    fun shootBackAsteroid() {
+        TODO("Реализовать shootBackAsteroid()")
+    }
+}
