0e4a33aa6c6d26d8352378b045d200ce61a7360e
Aleksandr Volynski <al.volynski@ariscard.net>
2025-04-24 16:36:14 +0300

    KS-18-5 fix

--- src/main/kotlin/lesson_18/lesson18_task5.kt
+++ src/main/kotlin/lesson_18/lesson18_task5.kt
@@ -2,28 +2,54 @@ package lesson_18
 
 fun main() {
     val screen = Screen()
-    screen.draw(10, 20)
-    screen.draw(100f, 200f)
-    screen.draw(400, 300f)
+    screen.drawCircle(10, 20)
+    screen.drawCircle(10f, 20f)
+
+    screen.drawSquare(100, 200)
+    screen.drawSquare(100f, 200f)
+
+    screen.drawPoint(400, 300)
+    screen.drawPoint(400f, 300f)
 }
 
 class Screen {
-    fun draw(
+    fun drawCircle(
         x: Int,
         y: Int,
     ) {
         println("Нарисован круг в ($x, $y)")
     }
 
-    fun draw(
+    fun drawCircle(
+        x: Float,
+        y: Float,
+    ) {
+        println("Нарисован круг в ($x, $y)")
+    }
+
+    fun drawSquare(
+        x: Int,
+        y: Int,
+    ) {
+        println("Нарисован квадрат в ($x, $y)")
+    }
+
+    fun drawSquare(
         x: Float,
         y: Float,
     ) {
         println("Нарисован квадрат в ($x, $y)")
     }
 
-    fun draw(
+    fun drawPoint(
         x: Int,
+        y: Int,
+    ) {
+        println("Нарисована точка в ($x, $y)")
+    }
+
+    fun drawPoint(
+        x: Float,
         y: Float,
     ) {
         println("Нарисована точка в ($x, $y)")
